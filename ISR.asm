e60735027a7d3e0eb25ec237422cf068ac5a334d
gwendolyntn <45651223+gwendolyntn@users.noreply.github.com>
2018-12-06 20:52:26 -0600

    Add files via upload

---
+++ ISR.asm
@@ -0,0 +1,50 @@
+; ISR.asm
+; Name:
+; UTEid: 
+; Keyboard ISR runs when a key is struck
+; Checks for a valid RNA symbol and places it at x4600
+               .ORIG x2600
+	ST R3, 	SAVE3
+	ST R4,	SAVE4
+	ST R5,	SAVE5
+
+	LDI R3, KBDR
+	AND R4, R4, #0
+	LD R5, A
+	ADD R4, R3, R5
+	BRz store
+	AND R4, R4, #0
+	LD R5, C
+	ADD R4, R3, R5
+	BRz store	
+	AND R4, R4, #0
+	LD R5, U
+	ADD R4, R3, R5
+	BRz store		
+	AND R4, R4, #0
+	LD R5, G
+	ADD R4, R3, R5
+	BRz store
+	BRnzp END	
+
+store	STI R3, GLOBAL
+
+END	LD R3, SAVE3
+	LD R4,	SAVE4
+	LD R5,	SAVE5
+
+	RTI
+
+SAVE3	.BLKW 1
+SAVE4	.BLKW 1
+SAVE5	.BLKW 1
+GLOBAL	.FILL	X4600
+STACK	.FILL	X4000
+KBIEN	.FILL	X4000
+KBDR	.FILL	XFE02
+KBSR	.FILL	XFE00
+A	.FILL	#-65
+C	.FILL	#-67
+U	.FILL	#-85
+G	.FILL	#-71
+		.END
---
+++ Main.asm
@@ -0,0 +1,105 @@
+; Main.asm
+; Name:
+; UTEid: 
+; Continuously reads from x4600 making sure its not reading duplicate
+; symbols. Processes the symbol based on the program description
+; of mRNA processing.
+               .ORIG x4000
+; initialize the stack pointer
+	LD R6, STACK
+
+; set up the keyboard interrupt vector table entry
+	LD R0, INT
+	STI R0, KBIVE
+
+; enable keyboard interrupts
+	LD R0, KBIEN
+	STI R0, KBSR
+
+	LD R0, ZERO
+	STI R0, BUFFER
+; start of actual program
+LOOP	LDI R0, BUFFER
+	BRz LOOP
+	TRAP X21
+	AND R1, R1, #0
+	STI R1, BUFFER
+	ADD R4, R4, #-1
+	BRz CHECKU
+	BRn CHECKA
+	BRp CHECKG
+	
+;Process R0
+CHECKA	AND R4, R4, #0
+	LD R2, A
+	ADD R3, R0, R2
+	BRnp LOOP
+	ADD R4, R4, #1
+	BRnzp LOOP
+CHECKU	LD R2, U
+	ADD R3, R0, R2
+	BRnp CHECKA
+	ADD R4, R4, #2
+	BRnzp LOOP
+CHECKG	LD R2, G
+	ADD R3, R0, R2
+	BRnp LOOP
+	LD R0, PIPE
+	TRAP X21
+	BRnzp next	
+
+
+;Check for stop codon
+next	AND R4, R4, #0
+loop	LDI R0, BUFFER
+	BRz loop
+	TRAP X21
+	AND R1, R1, #0
+	STI R1, BUFFER
+	ADD R4, R4, #-1
+	BRz checka
+	BRn checku
+	BRp checkA
+
+checku	AND R4, R4, #0
+	LD R2, U
+	ADD R3, R0, R2
+	BRnp loop
+	ADD R4, R4, #1
+	BRnzp loop
+checka	LD R2, A
+	ADD R3, R0, R2
+	BRnp checkg
+	ADD R4, R4, #2
+	BRnzp loop
+checkg	LD R2, G
+	ADD R3, R0, R2
+	BRnp loop
+	ADD R4, R4, #2
+	BRnzp loop
+
+checkA	LD R2, A
+	ADD R3, R0, R2
+	BRnp checkG
+	AND R4, R4, #0
+	TRAP X25
+checkG	LD R2, G
+	ADD R3, R0, R2
+	BRnp loop
+	AND R4, R4, #0
+	TRAP X25
+	
+ZERO	.FILL	0
+BUFFER	.FILL	X4600
+INT	.FILL	X2600
+STACK	.FILL	X4000
+KBIEN	.FILL	X4000
+KBSR	.FILL	XFE00
+KBDR	.FILL	XFE02
+KBIVE	.FILL	X0180
+A	.FILL	#-65
+C	.FILL	#-67
+U	.FILL	#-85
+G	.FILL	#-71
+PIPE	.FILL	#124
+		.END
